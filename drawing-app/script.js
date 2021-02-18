e0d81d0157f78507236d44035e855c0db90abac7
Sezer <sezermehmed98@gmail.com>
2021-02-18 20:00:03 +0300

    drawing app

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -47,7 +47,7 @@ function drawCircle(x, y) {
     ctx.fill()
 }
 
-function drawLine(x1, y2, x2, y2) {
+function drawLine(x1, y1, x2, y2) {
     ctx.beginPath()
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
