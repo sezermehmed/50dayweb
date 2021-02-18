594c0198cfc56a50bd2c81ee5fd10b468eb1c239
Sezer <sezermehmed98@gmail.com>
2021-02-18 09:33:20 +0300

    drag v1

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -7,10 +7,10 @@ buttons.forEach(button => {
 
         const buttonTop = e.target.offsetTop
         const buttonLeft = e.target.offsetLeft
-
+        console.log(buttonTop, buttonLeft)
         const xInside = x - buttonLeft
         const yInside = y - buttonTop
-
+        console.log(xInside, yInside)
         const circle = document.createElement('span')
         circle.classList.add('circle')
         circle.style.top = yInside + 'px'
